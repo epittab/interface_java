a59233aff920766041ebae82f0ed8eabac9f15b2
epittab <ep7m@me.com>
2020-11-26 14:49:06 -0500

    first commit

---
+++ Telephone/HomePhone.java
@@ -0,0 +1,37 @@
+package com.Interface.Telephone;
+
+public class HomePhone implements ITelephone {
+   
+    private int myNumber;
+    private boolean isRinging;
+
+    @Override
+    public void powerOn() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void answer() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isRinging() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
